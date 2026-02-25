a56220c42dd87dd42bc88b2b5b1f860d86b22a55
FR-Rithik <frrithik@gmail.com>
2026-02-26 05:18:44 +0600

    Day 1: FastAPI health check running in Docker

---
+++ app/main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {'message': 'you are viewing the homepage'}
+@app.get('/health')
+def health():
+    return {'status': 'ok'}
